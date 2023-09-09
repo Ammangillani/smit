ec543c5dc35d06a90940ce5de24e999806c871bd
Amman Gillani <ammangillani4321@icloud.com>
2023-09-09 11:36:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,402 @@
+// alert("hi im amman")
+// console.log("hi imah")
+// prompt("hi im html");
+// console.log(Math.random()*10)
+// let firstname=prompt("enter your name:")
+// let lastname="shah"
+// let age="23"
+// console.log("firstname:","age is", age)
+// console.log("lasrname:",lastname)
+// let name ="zubair";
+// name="shaikh"
+// console.log(name)
+// con
+
+
+// let favproglang =prompt("enter your favourite language")
+// let message=`My fav prog language is ${favproglang}`;
+// console.log (message)
+
+// let bigNr = 90071992547409920n;
+
+// let str1 = "JavaScript is fun!";
+// let str2 = "JavaScript is fun!";
+// console.log("These two strings are the same:", str1 == str2);
+
+
+// let unassigned;
+// console.log(unassigned);
+
+// let age = "value"
+
+// let petname=prompt("enter yourname");
+
+// let maname=prompt("enter your name");
+
+
+// console.log(petname is equal to maname === )
+
+
+// let name = "zubair";
+// let age = 23;
+// let married = false;
+// let gender = undefined
+// let salary = null
+// let saylanisalary =344444444444444444444n;
+// let identity = Symbol('2345');
+
+
+// console.log(typeof(name))
+// console.log(typeof(age))
+// console.log(typeof(married))
+// console.log(typeof(salary))
+// console.log(typeof(saylanisalary))
+
+
+// let nr1 = 2;
+// let nr2 ="2";
+// let nr3 = "a";
+
+
+
+// console.log(typeof String (nr1))
+// console.log(typeof Number(nr2) )
+// console.log(typeof Boolean (nr3))
+// console.log(Boolean (nr1))
+// console.log(Boolean (nr3))
+
+// let nr1 = 4;
+//  nr1++;
+//  console.log(nr1) ;
+
+// let nr = 2; 
+
+// let r= nr++;
+
+// console.log("nr:" , nr)
+
+
+// console.log(nr++);
+
+// console.log(nr);
+
+// let nr1 = 4;
+// let nr2 = 5;
+// let nr3 = 2;
+// let num4 = (nr1++ + ++nr2 * nr3++);
+// console.log(num4)
+
+// let value1 = +prompt("enter your value of a")
+// let value2 = +prompt("enter your value of b")
+     
+//      let eqauls = (value1) +  (value2);
+//      console.log(eqauls**0.5)
+
+// let a = 2;
+// let b = 4;
+// let c = 6 ;
+// a += b;
+// a /= c;
+// c %= b
+// console.log(a, b, c);
+
+
+                            //   **       // LOGICAL OPERATOTRS 
+
+                            // for and = &&
+                            // for or = ||
+                            // for not = !
+
+// let x = 6;
+// let y = 6;
+// console.log(y >= x); 
+
+// let x = 1;
+// let y = 2;
+// let z = 3;
+// let c = 4;
+// console.log(x < y || y > z || c < x);
+
+
+// let x = false;
+// let y = true;
+// console.log(!true , !false)
+
+// let a = "Hello";
+// a = "world";
+// console.log(a);
+
+// let a = "world";
+// let b = `Hello ${a}!`;
+// console.log(b);
+
+// let a = "Hello";
+// a = prompt("world");
+// console.log(a);
+
+// let a = 5;
+// let b = 70;
+// let c = "5";
+// ++b;
+// console.log(b);
+
+// let result = 3 + 4 * 2 / 8; 
+
+// console.log(result)
+
+// let firstNum = 5;
+// let secondNum = 10;
+// firstNum++;
+// secondNum--;
+// let total = ++firstNum + secondNum;
+// console.log(total);
+// let total2 = 500 + 100 / 5 + total--;
+// console.log(total2);
+
+// const a = 5;
+// const b = 10;
+// console.log(a > 0 && b > 0); 
+// console.log(a == 5 && b == 4); 
+// console.log(true ||false); 
+// console.log(a == 3 || b == 10); 
+// console.log(a == 3 || b == 7); 
+
+// let  value = +prompt("give a value in miles")
+// let  alertt = (value * 1.60934)
+// alert(alertt)
+
+
+// let a = +prompt("enter your height in feet")
+// let b = +prompt("enter your weigtht in pound")
+// let c = a*0.304 ; // for convert into meter
+// let d = b*0.453; //for convert into kg
+// let total = d/c*2; //formula
+// alert(`your bmi is ${total}`);
+
+
+//=========================logic statement===============================
+
+
+// let rain = true;
+// if(rain  === ture) 
+// { console.log("** Taking my umbrella when I need to go outside * " ) }
+//  else {
+//  console.log("** I can leave my umbrella at home **");
+// }
+
+
+// let age = +prompt("enter your age") //prompt
+// if(age < 18) { 
+//     console.log("We're very sorry, but you can't get in under 18")  // if
+//    } else {
+//     console.log("Welcome!");
+//    }
+   
+
+// let a = "" ;
+// if (a) {console.log("this is null")}
+// if (!a)  {console.log("this is undefined")}
+
+
+// let age = +prompt("enter your ege");
+// if (age < 18 ) {
+//     console.log("you are not allowed");
+// }
+//  else if (age === 18 ) {
+//     console.log("only one year left");
+// }
+// else {
+//     console.log("welcome");
+// }
+
+//===================PRACTICE ASSIGNMENT========================== 
+
+// let a=+prompt ("Enter your Numbers")
+// if (a >0 && a <40)
+
+// {console.log("you are failed")}
+
+
+//    else if (a >=40 && a <60)
+//     {console.log("Average")}
+
+//   else if (a >=60 && a <70)
+//   {console.log("you assigned B grade")}
+
+// else if (a >=70 && a <80)
+// {console.log("you assigned A grade")}
+
+// else if (a >=80 && a <90)h
+// {console.log("You Assigned A++")}
+
+// else 
+// {console.log("You enter wrong DAta")}
+
+
+//=======================Conditional ternary operators=====================//
+
+// let hobby ="dancing";
+
+
+
+// console.log(hobby === "codding" ? "i love codding" : "can u teach me that")
+
+
+
+// let age = 18;
+
+
+
+// console.log(age === 16 ? "yes 16" : age > 16 ? "yes greater then 16" : "its small")
+
+
+// let age = +prompt("what is your age")
+
+// console.log(age <= 13 ? "under age" : age > 16 ? "newbiew" : "franked")
+
+
+//===========================switch statement=================================//
+
+// if(activity === "Get up") {
+//     console.log("It is 6:30AM");
+//    } else if(activity === "Breakfast") {
+//     console.log("It is 7:00AM");
+//    } else if(activity === "Drive to work") {
+//     console.log("It is 8:00AM");
+//    } else if(activity === "Lunch") {
+//     console.log("It is 12.00PM");
+//    } else if(activity === "Drive home") {
+//     console.log("It is 5:00PM")
+//    } else if(activity === "Dinner") {
+//     console.log("It is 6:30PM");
+//    } 
+
+
+
+// let timing = +prompt("your timing");
+// // console.log(timing)
+// switch(timing) {
+  
+//     case "Get up":
+//     console.log("It is 6:30AM");
+//     break;
+//     case "Breakfast":
+//     console.log("It is 7:00AM");
+//     break;
+//     case "Drive to work":
+//     console.log("It is 8:00AM");
+//     break;
+//     case "Lunch":
+//     console.log("It is 12:00PM");
+//     break; 
+//     case "Drive home":
+//     console.log("It is 5:00PM");
+//     break; 
+//     case "Dinner":
+//     console.log("It is 6:30PM");
+//     break;
+//    };
+
+// let input =+prompt("Guess hidden number Hint : Between 0 to 10" )
+
+// let randomnumber =Math.random()
+
+// let c= b*c
+
+// let d= Math.floor(c);
+
+// switch(true){
+//     case input === randomnumber:
+//     alert("you won burj-khlifa")
+//     break;
+//     default:
+//         alert("better luck next time")
+// }
+
+// =====================================ARRAY==================================================
+
+// let arr = ["milk","bread", "apples"];
+
+// arr.splice(1,0,"orange") //splice for adding and removing item from array
+// arr.push("orange"); //,push for adding items to element
+// console.log(arr)
+ 
+// arr[1] ="banana"
+
+// console.log(arr)
+
+// let arr1 =[1, 2, 3];
+// let arr2 =[5, 4, 6];
+
+// let merge =arr1.concat(arr2) //concat for merging 2 array
+
+// console.log(arr1)
+
+// console.log(arr2)
+
+// console.log(merge)
+
+
+// let arr1 =[1, 2, 3];
+// let arr2 =[5, 4, 6];
+
+// let merge =arr1.concat(arr2)
+
+// console.log(arr1)
+
+// console.log(arr2)
+
+// console.log(merge)
+
+// merge.shift(); to del item from 1 position in array
+// console.log(merge) 
+
+// merge.unshift("zubair") to add item from 1 position in array
+// console.log(merge)
+
+
+// merge.pop(); // pop for deleting last item of the array
+// console.log(merge)
+
+// let name = ["zia" , "umer" , "ahsan", "jamal"];
+
+// console.log(name)
+
+// // name.sort(); // for sorting the elements numberwise and name wise
+// name.reverse(); // for rverseing the array
+
+// console.log(name)
+
+//=============================ARRAY PRACTICE EXCERSICE===============================================//
+
+// let x=["milk","bread","apples"];
+
+// x.splice(1,1,"bananas","egg")
+
+// console.log(x)
+
+// x.pop();
+
+// x.sort();
+
+// x.splice(1,0,"carrots" , "lettuce")
+
+// console.log(x)
+
+// let y=["juice","pop",];
+
+// let merge =x.concat (y)
+
+// console.log(merge)
+
+// =============================
+
+let num =[1,2,3,4,5,6,7,8,9,0,];
+
+console.log(num);
+
+let find =num.indexOf(1);
+
+let last = num.lastIndexOf(1);
+
+console.log(last)
