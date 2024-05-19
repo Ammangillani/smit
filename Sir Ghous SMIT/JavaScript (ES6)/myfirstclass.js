c124fe80bad3616464f9a4e7cb9757fc45a9f441
Amman Gillani <ammangillani4321@icloud.com>
2024-05-19 16:23:06 +0500

    commmit

--- Sir Ghous SMIT/JavaScript (ES6)/myfirstclass.js
+++ Sir Ghous SMIT/JavaScript (ES6)/myfirstclass.js
@@ -3,9 +3,18 @@
 // };
 // hello();
 
-setTimeout(() => {
-  console.log("hello");
-});
-Promise.resolve().then(() => console.log("hello2"));
+// setTimeout(() => {
+//   console.log("hello");
+// });
+// Promise.resolve().then(() => console.log("hello2"));
 
 // callstack
+function foo1() {
+  var a = "text";
+  function foo() {
+    var b = "test 1";
+    console.log(b);
+  }
+
+  foo();
+}
