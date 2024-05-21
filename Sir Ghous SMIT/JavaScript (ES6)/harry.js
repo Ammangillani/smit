bfaecfd0c53a9e35a241e17446ee8826904bb5e8
Amman Gillani <ammangillani4321@icloud.com>
2024-05-21 18:09:55 +0500

    commit

--- Sir Ghous SMIT/JavaScript (ES6)/harry.js
+++ Sir Ghous SMIT/JavaScript (ES6)/harry.js
@@ -1,1 +1,23 @@
-console.log("i am toturial on loop");
+// console.log("i am tutorial on loop");
+
+// let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+
+// types of loop {
+// -- for loop
+// -- for in loop
+// -- for of loop
+// -- for each loop
+// -- while loop
+// -- do while loop
+// }
+
+// for loop syntax and practice
+
+let a = 3;
+
+for (let i = 0; i < 100; i++) {
+  console.log(a + i);
+}
